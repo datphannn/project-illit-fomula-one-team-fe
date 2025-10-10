f3541b2237c850f343dc325fb0ccbf85e2b12f70
Đạt Phan <phangiadat2005@gmail.com>
2025-10-10 20:51:19 +0700

    Results page 1

--- app/(routes)/results/page.tsx
+++ app/(routes)/results/page.tsx
@@ -11,19 +11,7 @@ export default function ResultsPage() {
         <h1 className="text-4xl font-bold mb-6 border-b border-gray-700 pb-4">
           RESULTS
         </h1>
-        <div className="mb-6">
-          <select className="bg-gray-800 text-white p-2 rounded border border-gray-700">
-            <option value="2025">2025</option>
-            <option value="2024">2024</option>
-            <option value="2023">2023</option>
-            <option value="2022">2022</option>
-            <option value="2021">2021</option>
-            <option value="2020">2020</option>
-            <option value="2019">2019</option>
-            <option value="2018">2018</option>
-            <option value="2017">2017</option>
-          </select>
-        </div>
+
         <ResultsSection races={mockRacesDetailed} />
       </div>
     </div>
